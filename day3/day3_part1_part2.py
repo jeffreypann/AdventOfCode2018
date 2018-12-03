de1004ff08657253914e398682ecdef718d2842d
Jeffrey Pan <jeffreypann@gmail.com>
2018-12-03 16:31:04 -0500

    updated

--- day3/day3_part1_part2.py
+++ day3/day3_part1_part2.py
@@ -4,9 +4,8 @@ import numpy as np
 with open("day3_data.txt") as f:
     data = f.readlines()  # turn lines into array
 claims = map(lambda s: map(int, re.findall(r'-?\d+', s)), data)
-count = 0
 fab = np.zeros((1000,1000))
-test = np.zeros((5,5))
+
 for claim, x, y, dx, dy in claims:
     fab[x:x+dx,y:y+dy] += 1
 print(np.sum(fab>1))
